0b0dad5810973bf0de065bae2fb035ab0b05e1b2
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-02-16 15:42:39 +0530

    added EmpWage

---
+++ Employee Wage Computation/IComputeEmpWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Day10;
+
+public interface IComputeEmpWage {
+    void addCompanyEmpWage(String company, int empRatePerHr,
+                           int numOfWorkingDays, int maxHrsPerMonth);
+    void computeEmpWage();
+    int getTotalWage(String company);
+}
+
